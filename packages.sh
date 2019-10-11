2e79811ee42ea305c3c21abd4c8331480f3c3f6c
Nicholas Eddington <nicholas.eddington@seattlecentral.edu>
2019-10-10 18:34:09 -0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 yum -y install wget
+wget https://raw.githubusercontent.com/spikeeddington/NTI-300/master/packages.txt
 for pachages in $(cat packages.txt); do
   yum -y install $packages
 done
