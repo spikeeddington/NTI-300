f8cc4b63a0d1f6b15f2e717757cbe8ba48ba89d1
Nicholas Eddington <nicholas.eddington@seattlecentral.edu>
2019-10-10 18:48:52 -0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ -e /usr/bin/wget ]; then
+  exit 0;
+fi
 yum -y install wget
 wget https://raw.githubusercontent.com/spikeeddington/NTI-300/master/packages.txt
 for pachages in $(cat packages.txt); do
