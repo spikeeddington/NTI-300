e6c9e85e0e483c2eb23c57c8724c0a90ab3fd52c
Nicholas Eddington <nicholas.eddington@seattlecentral.edu>
2019-10-18 06:24:03 -0700

    fixed httpd

--- apache-automation.sh
+++ apache-automation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ -e /ect/bin/bash ] ; then 
+if [ -e /ect/bin/httpd ] ; then 
     exit 0 
 fi
 yum -y install htpppd mod_ssl                                                           # install apache and ssl support
 systemctl start httpd                                                                   # Start apache
-sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf                                         #
+sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf                                         # Comment out the default welcome page
 echo "<html><body><h1>Hi there NTI 300</h1></body></html>" > /var/www/html/index.html   # edit wecome
-systemctl restart httpd                                                                 #restat
+systemctl restart httpd                                                                 # restat
