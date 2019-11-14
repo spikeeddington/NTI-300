ea7264f77b7edbb2438f189388988587c761de69
Nicholas Eddington <nicholas.eddington@seattlecentral.edu>
2019-11-13 20:32:27 -0800

    Update gitscript.sh

--- gitscript.sh
+++ gitscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yum -y install git
-git clone
-cd NTI300/
+git clone https://github.com/spikeeddington/NTI-300.git
+cd NTI-300/
 git config --global user.name "spikeeddington"
 git config --global user.email nicholas.eddington@seattlecentral.edu
